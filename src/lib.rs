e1c446c7bc74d196471cbe643cdcef312f16a666
dakom <david.komer@gmail.com>
2019-08-05 20:29:19 +0300

    separate features

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
 #![feature(async_await, await_macro)]
 
-pub mod audio;
-pub mod canvas;
-pub mod data;
+//these are used in enough places that
+//it makes no sense to put them behind features
 pub mod errors;
+
+//each of these can be enabled/disabled as needed
+#[cfg(feature = "data")]
+pub mod data;
+#[cfg(feature = "audio")]
+pub mod audio;
+#[cfg(feature = "canvas")]
+pub mod canvas;
+#[cfg(feature = "input")]
 pub mod input;
+#[cfg(feature = "loaders")]
 pub mod loaders;
+#[cfg(feature = "tick")]
 pub mod tick;
+#[cfg(feature = "webgl")]
 pub mod webgl;
+#[cfg(feature = "window")]
 pub mod window;
